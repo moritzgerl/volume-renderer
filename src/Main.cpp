1264f871c0eb8f2075fd73c18364dcf57b03e841
Moritz Gerl <moritzgerl@gmail.com>
2025-11-05 12:48:09 +0100

    Removed copying the depth buffer.

--- src/Main.cpp
+++ src/Main.cpp
@@ -73,13 +73,6 @@ int main()
             renderPass.Render();
         }
 
-        // Copy depth buffer
-        // TODO check if we need this
-        ssaoInputFrameBuffer.Bind();
-        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-        glBlitFramebuffer(0, 0, Config::windowWidth, Config::windowHeight, 0, 0, Config::windowWidth, Config::windowHeight, GL_DEPTH_BUFFER_BIT, GL_NEAREST);
-        ssaoInputFrameBuffer.Unbind();
-
         if (displayProperties.showGui)
         {
             gui.Draw();
