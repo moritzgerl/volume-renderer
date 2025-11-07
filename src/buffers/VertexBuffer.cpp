4d4cc1d87720eba3659f9910835d0b012a3c42f1
Moritz Gerl <moritzgerl@gmail.com>
2025-11-07 19:19:03 +0100

    Unbound vertex buffer after initialization

--- src/buffers/VertexBuffer.cpp
+++ src/buffers/VertexBuffer.cpp
@@ -24,6 +24,9 @@ VertexBuffer::VertexBuffer(const ScreenQuadVertexCoordinates& screenQuadVertexCo
 
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 VertexBuffer::VertexBuffer(const UnitCubeVertexCoordinates& unitCubeVertexCoordinates)
@@ -47,6 +50,9 @@ VertexBuffer::VertexBuffer(const UnitCubeVertexCoordinates& unitCubeVertexCoordi
 
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 VertexBuffer::VertexBuffer(VertexBuffer&& other) noexcept
