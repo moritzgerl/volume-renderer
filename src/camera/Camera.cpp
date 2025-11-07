b62732ca9fa2e1fde1baefb8766c2416ecb344f5
Moritz Gerl <moritzgerl@gmail.com>
2025-11-07 14:29:48 +0100

    Removed unused constructor

--- src/camera/Camera.cpp
+++ src/camera/Camera.cpp
@@ -9,15 +9,6 @@
 #include <iostream>
 #include <iomanip>
 
-Camera::Camera(glm::vec3 position, glm::vec3 target, glm::vec3 up)
-    : m_zoom(Config::defaultZoom)
-{
-    m_position = position;
-    m_worldUp = up;
-    m_front = glm::normalize(target - position);
-    updateCameraVectors();
-}
-
 Camera::Camera(float posX, float posY, float posZ, float targetX, float targetY, float targetZ, float upX, float upY, float upZ)
     : m_zoom(Config::defaultZoom)
 {
