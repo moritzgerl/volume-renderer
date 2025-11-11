dbd3f63bb765cc31b54f9c408196bbf4b4a36afd
Moritz Gerl <moritzgerl@gmail.com>
2025-11-11 11:13:13 +0100

    Made ParseElementIndex more robust

--- src/persistence/ParseElementIndex.cpp
+++ src/persistence/ParseElementIndex.cpp
@@ -2,30 +2,46 @@
 #include <persistence/ParseValue.h>
 #include <persistence/ApplicationStateIniFileSection.h>
 
+namespace
+{
+    constexpr std::string_view transferFunctionPointPrefix = "[TransferFunctionPoint";
+    constexpr std::string_view pointLightPrefix = "[PointLight";
+}
+
 std::expected<unsigned int, Persistence::ApplicationStateIniFileLoadingError> Persistence::ParseElementIndex(const std::string_view line, Persistence::ApplicationStateIniFileSection currentSection)
 {
-    std::string_view elementIndexString;
-    
+    std::string_view prefix;
+
     switch (currentSection)
     {
         case ApplicationStateIniFileSection::TransferFunctionPoint:
-        {
-            // TODO make robust
-            elementIndexString = line.substr(22, line.size() - 23);
+            prefix = transferFunctionPointPrefix;
             break;
-        }
         case ApplicationStateIniFileSection::PointLight:
-        {
-            // TODO make robust
-            elementIndexString = line.substr(11, line.size() - 12);
+            prefix = pointLightPrefix;
             break;
-        }
         default:
-        {
             return std::unexpected(ApplicationStateIniFileLoadingError::ParseError);
-        }
-    }    
-    
+    }
+
+    if (line.size() <= prefix.size() + 1)
+    {
+        return std::unexpected(ApplicationStateIniFileLoadingError::ParseError);
+    }
+
+    size_t closingBracketPos = line.find(']', prefix.size());
+    if (closingBracketPos == std::string_view::npos)
+    {
+        return std::unexpected(ApplicationStateIniFileLoadingError::ParseError);
+    }
+
+    std::string_view elementIndexString = line.substr(prefix.size(), closingBracketPos - prefix.size());
+
+    if (elementIndexString.empty())
+    {
+        return std::unexpected(ApplicationStateIniFileLoadingError::ParseError);
+    }
+
     auto parseValueResult = ParseValue<unsigned int>(elementIndexString);
 
     if (parseValueResult)
