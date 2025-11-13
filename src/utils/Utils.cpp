a0c9ed78f119cb96905f65b35305b7accc9009e9
Moritz Gerl <moritzgerl@gmail.com>
2025-11-13 14:16:37 +0100

    Removed Utils.h / .cpp

--- src/utils/Utils.cpp
+++
@@ -1,8 +0,0 @@
-#include <utils/Utils.h>
-
-void Utils::AddPointToCoordinates(const glm::vec3& point, std::vector<float>& coordinates)
-{
-    coordinates.push_back(point.x);
-    coordinates.push_back(point.y);
-    coordinates.push_back(point.z);
-}
