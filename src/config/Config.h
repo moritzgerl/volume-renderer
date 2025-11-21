55d4062929dfdbf370fc65d433792f0a34505580
Moritz Gerl <moritzgerl@gmail.com>
2025-11-21 09:05:56 +0100

    Changed scalar constants in Config.h from const to constexpr

--- src/config/Config.h
+++ src/config/Config.h
@@ -37,33 +37,33 @@
 */
 namespace Config
 {
-    const unsigned int windowWidth = 1920;
-    const unsigned int windowHeight = 1080;
-    //const unsigned int windowWidth = 3840;
-    //const unsigned int windowHeight = 2160;
+    constexpr unsigned int windowWidth = 1920;
+    constexpr unsigned int windowHeight = 1080;
+    //constexpr unsigned int windowWidth = 3840;
+    //constexpr unsigned int windowHeight = 2160;
     const std::filesystem::path applicationStateIniFilePath = "./volume-renderer.ini";
     const std::filesystem::path datasetPath = "./datasets/knee.raw";
-    const bool showLightSourceByDefault = false;
-    const float defaultGuiWidthRatio = 0.3f;
-    const float defaultTransferFunctionGuiHeightRatio = 0.3f;
-    const unsigned int numPointLights = 2;
+    constexpr bool showLightSourceByDefault = false;
+    constexpr float defaultGuiWidthRatio = 0.3f;
+    constexpr float defaultTransferFunctionGuiHeightRatio = 0.3f;
+    constexpr unsigned int numPointLights = 2;
     const DirectionalLight defaultDirectionalLight = Factory::MakeDefaultDirectionalLight();
     const std::vector<PointLight> defaultPointLights = Factory::MakeDefaultPointLights(numPointLights);
     // move to its own header, use constexpr
     const TransferFunction defaultTransferFunction = Factory::MakeDefaultTransferFunction();
-    const unsigned int defaultSsaoKernelSize = 64;
-    const unsigned int defaultSsaoNoiseSize = 4;
-    const float defaultSsaoRadius = 0.538f;
-    const float defaultSsaoBias = 0.028f;
-    const glm::vec3 defaultCameraPosition = glm::vec3(1.1f, 0.73f, 1.1f);
-    const glm::vec3 defaultCameraLookAt = glm::vec3(0.0f, 0.0f, 0.0f);
-    const glm::vec3 defaultCameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-    const float defaultCameraZoom = 45.0f;
-    const float trackballSensitivityMin = 0.0005f;
-    const float trackballSensitivityMax = 0.005f;
-    const float defaultTrackballSensitivity = 0.003f;
-    const bool defaultTrackballInvertYAxis = true;
-    const float defaultRaycastingDensityMultiplier = 20.0f;
+    constexpr unsigned int defaultSsaoKernelSize = 64;
+    constexpr unsigned int defaultSsaoNoiseSize = 4;
+    constexpr float defaultSsaoRadius = 0.538f;
+    constexpr float defaultSsaoBias = 0.028f;
+    constexpr glm::vec3 defaultCameraPosition = glm::vec3(1.1f, 0.73f, 1.1f);
+    constexpr glm::vec3 defaultCameraLookAt = glm::vec3(0.0f, 0.0f, 0.0f);
+    constexpr glm::vec3 defaultCameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+    constexpr float defaultCameraZoom = 45.0f;
+    constexpr float trackballSensitivityMin = 0.0005f;
+    constexpr float trackballSensitivityMax = 0.005f;
+    constexpr float defaultTrackballSensitivity = 0.003f;
+    constexpr bool defaultTrackballInvertYAxis = true;
+    constexpr float defaultRaycastingDensityMultiplier = 20.0f;
 }
 
 #endif
