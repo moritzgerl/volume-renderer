1a43b0c9a49832265bf441d8cdd0e8306c46cb8a
Moritz Gerl <moritzgerl@gmail.com>
2025-11-26 16:38:26 +0100

    Added sub-factory functions for the other render passes

--- src/renderpass/MakeRenderPasses.cpp
+++ src/renderpass/MakeRenderPasses.cpp
@@ -57,13 +57,13 @@ namespace
 
     RenderPass MakeRaycastingRenderPass(
         const Camera& camera,
-        const Gui& gui,
         const GuiParameters& guiParameters,
-        const InputHandler& inputHandler,
         const TextureStorage& textureStorage,
         const ShaderStorage& shaderStorage,
         const FrameBufferStorage& frameBufferStorage,
-        const UnitCube& unitCube
+        const UnitCube& unitCube,
+        float viewportWidth,
+        float viewportHeight
         )
     {
         auto textures = std::vector<std::reference_wrapper<const Texture>>{};
@@ -72,12 +72,10 @@ namespace
 
         const auto& shader = shaderStorage.GetElement(ShaderId::Volume);
 
-        auto prepareFunction = [&camera, &gui, &guiParameters, &inputHandler, &shader]()
+        auto prepareFunction = [&camera, &guiParameters, &shader, viewportWidth, viewportHeight]()
         {
             glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            const auto viewportWidth = static_cast<float>(inputHandler.GetWindowWidth() - static_cast<int>(gui.GetGuiWidth()));
-            const auto viewportHeight = static_cast<float>(inputHandler.GetWindowHeight());
             ShaderUtils::UpdateCameraMatricesInShader(camera, shader, viewportWidth, viewportHeight);
             shader.SetVec3("cameraPos", camera.GetPosition());
             shader.SetMat4("model", glm::mat4{ 1.0f });
@@ -99,40 +97,24 @@ namespace
             std::move(renderFunction)
         };
     }
-}
 
-
-RenderPasses Factory::MakeRenderPasses(const Gui& gui, const InputHandler& inputHandler, const Storage& storage)
-{
-    const auto& camera = storage.GetCamera();
-    const auto& displayProperties = storage.GetDisplayProperties();
-    const auto& guiParameters = storage.GetGuiParameters();
-    const auto& ssaoKernel = storage.GetSsaoKernel();
-    const auto& screenQuad = storage.GetScreenQuad();
-    const auto& unitCube = storage.GetUnitCube();
-    const auto& textureStorage = storage.GetTextureStorage();
-    const auto& shaderStorage = storage.GetShaderStorage();
-    const auto& frameBufferStorage = storage.GetFrameBufferStorage();
-
-    RenderPasses renderPasses =
-    {
-        MakeSetupRenderPass(gui, inputHandler, shaderStorage, frameBufferStorage),
-        MakeRaycastingRenderPass(camera, gui, guiParameters, inputHandler, textureStorage, shaderStorage, frameBufferStorage, unitCube)
-    };
-
-/*
-    // SSAO Input
-    // TODO - remove?
+    RenderPass MakeSsaoInputRenderPass(
+        const Camera& camera,
+        const ShaderStorage& shaderStorage,
+        const FrameBufferStorage& frameBufferStorage,
+        const UnitCube& unitCube,
+        float viewportWidth,
+        float viewportHeight)
     {
         std::vector<std::reference_wrapper<const Texture>> textures;
 
         const auto& shader = shaderStorage.GetElement(ShaderId::SsaoInput);
 
-        auto prepareFunction = [&camera, &shader]()
+        auto prepareFunction = [&camera, &shader, viewportWidth, viewportHeight]()
         {
             glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            ShaderUtils::UpdateCameraMatricesInShader(camera, shader);
+            ShaderUtils::UpdateCameraMatricesInShader(camera, shader, viewportWidth, viewportHeight);
         };
 
         auto renderFunction = [&unitCube]()
@@ -140,17 +122,25 @@ RenderPasses Factory::MakeRenderPasses(const Gui& gui, const InputHandler& input
             unitCube.Render();
         };
 
-        renderPasses.emplace_back(
+        return
+        {
             RenderPassId::SsaoInput,
             shader,
             frameBufferStorage.GetElement(FrameBufferId::SsaoInput),
             std::move(textures),
             std::move(prepareFunction),
             std::move(renderFunction)
-        );
+        };
     }
 
-    // SSAO
+    RenderPass MakeSsaoRenderPass(
+        const Camera& camera,
+        const TextureStorage& textureStorage,
+        const ShaderStorage& shaderStorage,
+        const FrameBufferStorage& frameBufferStorage,
+        const ScreenQuad& screenQuad,
+        float viewportWidth,
+        float viewportHeight)
     {
         std::vector<std::reference_wrapper<const Texture>> textures;
         // TODO use { }
@@ -160,9 +150,9 @@ RenderPasses Factory::MakeRenderPasses(const Gui& gui, const InputHandler& input
 
         const auto& shader = shaderStorage.GetElement(ShaderId::Ssao);
 
-        auto prepareFunction = [&camera, &shader]()
+        auto prepareFunction = [&camera, &shader, viewportWidth, viewportHeight]()
         {
-            ShaderUtils::UpdateCameraMatricesInShader(camera, shader);
+            ShaderUtils::UpdateCameraMatricesInShader(camera, shader, viewportWidth, viewportHeight);
             glClear(GL_COLOR_BUFFER_BIT);
         };
 
@@ -171,17 +161,22 @@ RenderPasses Factory::MakeRenderPasses(const Gui& gui, const InputHandler& input
             screenQuad.Render();
         };
 
-        renderPasses.emplace_back(
+        return
+        {
             RenderPassId::Ssao,
             shader,
             frameBufferStorage.GetElement(FrameBufferId::Ssao),
             std::move(textures),
             std::move(prepareFunction),
             std::move(renderFunction)
-        );
+        };
     }
 
-    // SSAO Blur
+    RenderPass MakeSsaoBlurRenderPass(
+        const TextureStorage& textureStorage,
+        const ShaderStorage& shaderStorage,
+        const FrameBufferStorage& frameBufferStorage,
+        const ScreenQuad& screenQuad)
     {
         std::vector<std::reference_wrapper<const Texture>> textures;
         // TODO use { }
@@ -197,17 +192,23 @@ RenderPasses Factory::MakeRenderPasses(const Gui& gui, const InputHandler& input
             screenQuad.Render();
         };
 
-        renderPasses.emplace_back(
+        return
+        {
             RenderPassId::SsaoBlur,
             shaderStorage.GetElement(ShaderId::SsaoBlur),
             frameBufferStorage.GetElement(FrameBufferId::SsaoBlur),
             std::move(textures),
             std::move(prepareFunction),
             std::move(renderFunction)
-        );
+        };
     }
 
-    // SSAO Final
+    RenderPass MakeSsaoFinalRenderPass(
+        const GuiParameters& guiParameters,
+        const TextureStorage& textureStorage,
+        const ShaderStorage& shaderStorage,
+        const FrameBufferStorage& frameBufferStorage,
+        const ScreenQuad& screenQuad)
     {
         std::vector<std::reference_wrapper<const Texture>> textures;
         // TODO use { }
@@ -232,27 +233,34 @@ RenderPasses Factory::MakeRenderPasses(const Gui& gui, const InputHandler& input
             glDepthMask(GL_TRUE);
         };
 
-        renderPasses.emplace_back(
+        return
+        {
             RenderPassId::SsaoFinal,
             shader,
             frameBufferStorage.GetElement(FrameBufferId::Default),
             std::move(textures),
             std::move(prepareFunction),
             std::move(renderFunction)
-        );
+        };
     }
 
-    // Light Source
+    RenderPass MakeLightSourceRenderPass(
+        const Camera& camera,
+        const GuiParameters& guiParameters,
+        const ShaderStorage& shaderStorage,
+        const FrameBufferStorage& frameBufferStorage,
+        float viewportWidth,
+        float viewportHeight)
     {
         std::vector<std::reference_wrapper<const Texture>> textures;
 
         const auto& shader = shaderStorage.GetElement(ShaderId::LightSource);
 
-        auto prepareFunction = [&camera, &shader]()
+        auto prepareFunction = [&camera, &shader, viewportWidth, viewportHeight]()
         {
             glEnable(GL_BLEND);
             glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-            ShaderUtils::UpdateCameraMatricesInShader(camera, shader);
+            ShaderUtils::UpdateCameraMatricesInShader(camera, shader, viewportWidth, viewportHeight);
         };
 
         auto renderFunction = [&guiParameters, &shaderStorage]()
@@ -267,17 +275,23 @@ RenderPasses Factory::MakeRenderPasses(const Gui& gui, const InputHandler& input
             }
         };
 
-        renderPasses.emplace_back(
+        return
+        {
             RenderPassId::LightSource,
             shader,
             frameBufferStorage.GetElement(FrameBufferId::Default),
             std::move(textures),
             std::move(prepareFunction),
             std::move(renderFunction)
-        );
+        };
     }
 
-    // Debug
+    RenderPass MakeDebugRenderPass(
+        const DisplayProperties& displayProperties,
+        const TextureStorage& textureStorage,
+        const ShaderStorage& shaderStorage,
+        const FrameBufferStorage& frameBufferStorage,
+        const ScreenQuad& screenQuad)
     {
         std::vector<std::reference_wrapper<const Texture>> textures;
         // TODO use { }
@@ -301,16 +315,43 @@ RenderPasses Factory::MakeRenderPasses(const Gui& gui, const InputHandler& input
             }
         };
 
-        renderPasses.emplace_back(
+        return
+        {
             RenderPassId::Debug,
             shader,
             frameBufferStorage.GetElement(FrameBufferId::Default),
             std::move(textures),
             std::move(prepareFunction),
             std::move(renderFunction)
-        );
+        };
     }
-*/
-    return renderPasses;
 }
 
+
+RenderPasses Factory::MakeRenderPasses(const Gui& gui, const InputHandler& inputHandler, const Storage& storage)
+{
+    const auto& camera = storage.GetCamera();
+    const auto& displayProperties = storage.GetDisplayProperties();
+    const auto& guiParameters = storage.GetGuiParameters();
+    const auto& ssaoKernel = storage.GetSsaoKernel();
+    const auto& screenQuad = storage.GetScreenQuad();
+    const auto& unitCube = storage.GetUnitCube();
+    const auto& textureStorage = storage.GetTextureStorage();
+    const auto& shaderStorage = storage.GetShaderStorage();
+    const auto& frameBufferStorage = storage.GetFrameBufferStorage();
+
+    const auto viewportWidth = static_cast<float>(inputHandler.GetWindowWidth() - static_cast<int>(gui.GetGuiWidth()));
+    const auto viewportHeight = static_cast<float>(inputHandler.GetWindowHeight());
+
+    return
+    {
+        MakeSetupRenderPass(gui, inputHandler, shaderStorage, frameBufferStorage),
+        MakeRaycastingRenderPass(camera, guiParameters, textureStorage, shaderStorage, frameBufferStorage, unitCube, viewportWidth, viewportHeight),
+        //MakeSsaoInputRenderPass(camera, shaderStorage, frameBufferStorage, unitCube, viewportWidth, viewportHeight),
+        //MakeSsaoRenderPass(camera, textureStorage, shaderStorage, frameBufferStorage, screenQuad, viewportWidth, viewportHeight),
+        //MakeSsaoBlurRenderPass(textureStorage, shaderStorage, frameBufferStorage, screenQuad),
+        //MakeSsaoFinalRenderPass(guiParameters, textureStorage, shaderStorage, frameBufferStorage, screenQuad),
+        //MakeLightSourceRenderPass(camera, guiParameters, shaderStorage, frameBufferStorage, viewportWidth, viewportHeight),
+        //MakeDebugRenderPass(displayProperties, textureStorage, shaderStorage, frameBufferStorage, screenQuad)
+    };
+}
